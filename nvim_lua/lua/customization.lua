c8997cee9838484dddec50177888712c6fa8aee7
shlomo <shlomo.carmeter@gmail.com>
2021-06-14 10:44:19 +0300

    nvim: lua fixed colorscheme to match vimscript

--- nvim_lua/lua/customization.lua
+++ nvim_lua/lua/customization.lua
@@ -45,11 +45,4 @@ lspconfig.tsserver.setup {
 function _vifm_toggle()
   vifm:toggle()
 end
- -- local function t(str)
-     -- return vim.api.nvim_replace_termcodes(str, true, true, true)
- -- end
- -- 
- -- function _G.smart_tab()
-     -- return vim.fn.pumvisible() == 1 and t'<C-n>' or t'<Tab>'
- -- end
 
--- nvim_lua/lua/scheme.lua
+++ nvim_lua/lua/scheme.lua
@@ -1,1 +1,28 @@
-vim.g.colors_name = 'dracula'
+vim.cmd([[silent! colorscheme dracula]])
+
+vim.cmd([[
+    augroup ChangeBackgroudColour
+        autocmd colorscheme * :hi normal guibg=#000000 guifg=#2DE1FC
+        autocmd colorscheme * :hi PMenu guibg=#2176FF guifg=#ffffff 
+        autocmd colorscheme * :hi PMenuSel guibg=#ffffff guifg=#000000 
+        autocmd colorscheme * :hi Comment guifg=#34a1eb
+        autocmd colorscheme * :hi CursorLine guibg=#235789 ctermbg=100 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi CursorLineNr guifg=#ffff00 ctermfg=248 guibg=#112630 ctermbg=235 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi Folded guibg=#34a1eb guifg=#000000 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi GitGutterAdd guibg=#009900 guifg=#ffffff ctermfg=2
+        autocmd colorscheme * :hi GitGutterChange guibg=#bbbb00 guifg=#ffffff ctermfg=3
+        autocmd colorscheme * :hi GitGutterDelete guibg=#ff2222 guifg=#ffffff ctermfg=1
+        autocmd colorscheme * :hi HighlightedyankRegion guifg=#ffffff guibg=#129AE2
+        autocmd colorscheme * :hi IncSearch guifg=#000000 guibg=#ffff00 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi LineNr guifg=#2DE1FC ctermfg=238 guibg=#000000 ctermbg=233 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi MatchParen cterm=none guibg=#129AE2 guifg=#ffffff
+        autocmd colorscheme * :hi Search guibg=#ffff00 guifg=#000000
+        autocmd colorscheme * :hi String guifg=#34a1eb ctermfg=222 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi javaScriptLineComment guifg=#34a1eb gui=italic
+        autocmd colorscheme * :hi javaScriptStringS guifg=#2DE1FC ctermfg=222 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi jsComment guifg=#34a1eb gui=italic
+        autocmd colorscheme * :hi jsString guifg=#ffff00 ctermfg=222 gui=NONE cterm=NONE
+        autocmd colorscheme * :hi link Searchlight Folded
+    augroup END
+]])
+
