f01941403ec4999e01da78ac4956160f6a0bc2fa
shlomo <shlomo.carmeter@gmail.com>
2021-07-29 15:38:36 +0300

    nvim: lua added open multiple files to telescope

--- nvim_lua_new/lua/functions.lua
+++ nvim_lua_new/lua/functions.lua
@@ -16,11 +16,14 @@ function custom_actions.fzf_multi_select(prompt_bufnr)
   local num_selections = table.getn(picker:get_multi_selection())
 
   if num_selections > 1 then
-    actions.send_selected_to_qflist(prompt_bufnr)
-    actions.open_qflist()
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    for _, entry in ipairs(picker:get_multi_selection()) do
+      vim.cmd(string.format("%s %s", ":e!", entry.value))
+    end
   else
     actions.file_edit(prompt_bufnr)
   end
+  vim.cmd('stopinsert')
 end
 
 require('telescope').setup {
@@ -35,24 +38,24 @@ require('telescope').setup {
     file_ignore_patterns = { "node_modules", ".git" },
     mappings = { 
       i = {
-        ["<esc>"] = actions.close,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
-        ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-        ["<cr>"] = custom_actions.fzf_multi_select,
+        ['<esc>'] = actions.close,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<tab>'] = actions.toggle_selection + actions.move_selection_next,
+        ['<s-tab>'] = actions.toggle_selection + actions.move_selection_previous,
+        ['<cr>'] = custom_actions.fzf_multi_select,
       },
       n = {
-        ["<esc>"] = actions.close,
-        ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
-        ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-        ["<cr>"] = custom_actions.fzf_multi_select
+        ['<esc>'] = actions.close,
+        ['<tab>'] = actions.toggle_selection + actions.move_selection_next,
+        ['<s-tab>'] = actions.toggle_selection + actions.move_selection_previous,
+        ['<cr>'] = custom_actions.fzf_multi_select
       }
     },
   }
 }
 
-require'lualine'.setup {
+require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'dracula',
