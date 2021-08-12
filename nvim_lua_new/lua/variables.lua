0a3cb8c0f97c6447e8fa3012c68145a595a80c15
shlomo <shlomo.carmeter@gmail.com>
2021-08-12 14:54:50 +0300

    nvim: lua removed indent markers from nvim tree

--- nvim_lua_new/lua/variables.lua
+++ nvim_lua_new/lua/variables.lua
@@ -14,7 +14,6 @@ vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
-vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.vsnip_snippet_dir = '~/.config/nvim/snippets'
 vim.g.ctrlsf_auto_preview = 1
