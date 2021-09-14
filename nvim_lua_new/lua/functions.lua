7cb50ee7282c002e1a7d7cbeb25ad9cfe43cb37f
shlomo <shlomo.carmeter@gmail.com>
2021-09-14 15:07:16 +0300

    nvim: lua removed duplicated line on telescope open multi selection

--- nvim_lua_new/lua/functions.lua
+++ nvim_lua_new/lua/functions.lua
@@ -14,9 +14,7 @@ end
 function custom_actions.fzf_multi_select(prompt_bufnr)
   local picker = action_state.get_current_picker(prompt_bufnr)
   local num_selections = table.getn(picker:get_multi_selection())
-
   if num_selections > 1 then
-    local picker = action_state.get_current_picker(prompt_bufnr)
     for _, entry in ipairs(picker:get_multi_selection()) do
       vim.cmd(string.format("%s %s", ":e!", entry.value))
     end
