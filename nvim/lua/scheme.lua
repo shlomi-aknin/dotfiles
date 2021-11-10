47733791723983dadf3af111fd6de3d7550c36ea
shlomo <shlomo.carmeter@gmail.com>
2021-11-10 13:04:55 +0200

    nvim: added terminate debug key

--- nvim/lua/scheme.lua
+++ nvim/lua/scheme.lua
@@ -12,7 +12,7 @@ vim.cmd([[
   hi HopNextKey guifg=#50fa7b
   hi HopNextKey1 guifg=#50fa7b
   hi HopNextKey2 guifg=#50fa7b
-  hi IncSearch guifg=#000000 guibg=#FEA82F
+  hi IncSearch guifg=#000000 guibg=#50fa7b
   hi IndentEven guibg=NONE
   hi IndentOdd guibg=#001f33
   hi MatchParen guibg=#2176FF guifg=#ffffff gui=bold
@@ -21,7 +21,7 @@ vim.cmd([[
   hi NvimTreeFolderIcon guibg=blue
   hi PMenu guibg=#282a36 guifg=#ffffff
   hi PMenuSel guibg=#34a1eb guifg=#000000 blend=0
-  hi Search guifg=#000000 guibg=#FEA82F
+  hi Search guifg=#000000 guibg=#50fa7b
   hi SignColumn guibg=#000000
   hi String guifg=#34a1eb
   hi TabLine guibg=#000000 guifg=#ffff00 gui=NONE
