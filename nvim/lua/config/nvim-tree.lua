bb36796052b1e64247da40734fae5853be1a28ab
shlomo <shlomo.carmeter@gmail.com>
2021-11-11 14:53:29 +0200

    nvim: show folder name only on nvim tree

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -6,6 +6,7 @@ winopts.number = true
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_root_folder_modifier = ':t'
 
 require('nvim-tree').setup({
   open_on_setup       = false,
