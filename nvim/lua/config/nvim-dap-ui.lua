e6ff3d79cc4f76125b34b43f7a5eaf50fa50b9b9
shlomo <shlomo.carmeter@gmail.com>
2021-11-11 15:58:40 +0200

    nvim: rearranged dap ui

--- nvim/lua/config/nvim-dap-ui.lua
+++ nvim/lua/config/nvim-dap-ui.lua
@@ -12,19 +12,17 @@ require('dapui').setup({
     -- You can change the order of elements in the sidebar
     elements = {
       -- Provide as ID strings or tables with 'id' and 'size' keys
-      {
-        id = 'scopes',
-        size = 0.25, -- Can be float or integer > 1
-      },
       { id = 'breakpoints', size = 0.25 },
-      { id = 'stacks', size = 0.25 },
-      { id = 'watches', size = 00.25 },
+      { id = 'stacks', size = 0.65 },
+      -- { id = 'scopes', size = 0.45 },
+      { id = 'repl', size = 0.10 },
+      -- { id = 'watches', size = 0.25 },
     },
-    size = 40,
+    size = 20,
     position = 'left', -- Can be 'left', 'right', 'top', 'bottom'
   },
   tray = {
-    elements = { 'repl' },
+    elements = { 'scopes' },
     size = 10,
     position = 'bottom', -- Can be 'left', 'right', 'top', 'bottom'
   },
--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -51,4 +51,3 @@ require('telescope').setup({
     },
   }
 })
-
