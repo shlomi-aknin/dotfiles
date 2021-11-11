d83f941803258879c491596f5191f2b06de0dc28
shlomo <shlomo.carmeter@gmail.com>
2021-11-11 15:02:27 +0200

    nvim: added autocmd to expand folds one session restores

--- nvim/lua/vimscript.lua
+++ nvim/lua/vimscript.lua
@@ -1,14 +1,17 @@
 vim.cmd([[
   let $PATH .= ':/usr/local/lib/node_modules/bin'
-  au BufEnter *.{css,html,js,php} :norm zX<CR>
+  autocmd BufEnter *.{css,html,js,php} :norm zX<CR>
+  autocmd Syntax css,html,js,php normal zR
   autocmd BufReadPost quickfix nnoremap <buffer> <C-j> :QFNext<cr><C-w>j
   autocmd BufReadPost quickfix nnoremap <buffer> <C-k> :QFPrev<cr><C-w>j
   autocmd BufReadPost quickfix nnoremap <buffer> <C-l> <cr>
   autocmd BufReadPost quickfix nnoremap <buffer> <C-u> :call setqflist([])<cr>:cclose<cr>
+
   augroup highlight_yank
       autocmd!
       au TextYankPost * silent! lua vim.highlight.on_yank{ higroup='Folded', timeout=800 }
   augroup END
+
   augroup Format
       autocmd!
       autocmd BufWritePost * FormatWrite
