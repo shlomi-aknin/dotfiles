b73906a92932a0bf19c20f560ff2567770440994
shlomo <shlomo.carmeter@gmail.com>
2021-11-25 15:21:29 +0200

    nvim: added config to vim-vsnip

---
+++ nvim/lua/config/vim-vsnip.lua
@@ -0,0 +1,6 @@
+vim.cmd([[
+  imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+  smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+  imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+  smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+]])
