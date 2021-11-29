0e24c6ff24ac2c74aabd80bb511aad901d56d85b
shlomo <shlomo.carmeter@gmail.com>
2021-11-29 10:40:15 +0200

    nvim: changed pmenusel bgcolor

--- nvim/lua/scheme.lua
+++ nvim/lua/scheme.lua
@@ -20,7 +20,7 @@ vim.cmd([[
   hi Normal guifg=#ffffff guibg=#000000 gui=bold
   hi NormalSelected guifg=#50fa7b gui=bold
   hi NvimTreeFolderIcon guibg=blue
-  hi PMenu guibg=#1F1D36 guifg=#ffffff gui=bold
+  hi PMenu guibg=#00509d guifg=#ffffff gui=bold
   hi PMenuSel guibg=#34a1eb guifg=#000000 blend=0
   hi Search guifg=#000000 guibg=#ff5555
   hi SignColumn guibg=#000000 guifg=#34a1eb gui=bold
