814a81688833412a74a796d281205086908070b9
shlomo <shlomo.carmeter@gmail.com>
2022-02-13 12:55:09 +0200

    nvim: spectre config changed colors and mappings

--- nvim/lua/config/nvim-spectre.lua
+++ nvim/lua/config/nvim-spectre.lua
@@ -1,13 +1,26 @@
 require('spectre').setup({
+  open_cmd = 'enew',
   live_update = true,
   line_sep_start = '==========================================',
   result_padding = '~> ',
   line_sep       = '==========================================',
   highlight = {
       ui = "String",
-      search = "DiffChange",
-      replace = "DiffDelete"
+      search = "Search",
+      replace = "IncSearch"
   },
   is_open_target_win = true,
   is_insert_mode = true,
+  mapping = {
+    ['enter_file'] = {
+      map = "<leader>l",
+      cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
+      desc = "goto current file"
+    },
+    ['run_replace'] = {
+      map = "<leader>r",
+      cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+      desc = "replace all"
+    },
+  },
 })
