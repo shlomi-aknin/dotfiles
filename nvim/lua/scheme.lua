93ddd58184d789bd07344117f4472b0700013c73
shlomo <shlomo.carmeter@gmail.com>
2022-02-13 11:05:47 +0200

    nvim: use new set highlight function for color scheme

--- nvim/lua/scheme.lua
+++ nvim/lua/scheme.lua
@@ -1,35 +1,5 @@
 vim.cmd([[
   silent! colorscheme dracula
-  hi Comment guifg=#34a1eb gui=bold
-  hi CursorLine guibg=#00509d
-  hi CursorLineNr guifg=#ffff00 guibg=#000000
-  hi CursorWord gui=underline
-  hi Folded guibg=#34a1eb guifg=#000000
-  hi GitSignsAdd guifg=#50fa7b guibg=#000000 gui=bold
-  hi GitSignsChange guifg=#ff79c6 guibg=#000000 gui=bold
-  hi GitSignsDelete guifg=#ff5555 guibg=#000000 gui=bold
-  hi HopNextKey guifg=#50fa7b
-  hi HopNextKey1 guifg=#50fa7b
-  hi HopNextKey2 guifg=#50fa7b
-  hi IncSearch guibg=#000000 guifg=#50fa7b
-  hi IndentEven guibg=NONE
-  hi IndentOdd guibg=#001f33
-  hi LineNr guifg=#34a1eb guibg=#000000 gui=bold
-  hi LspSignatureActiveParameter guifg=#000000 guibg=#34a1eb gui=bold
-  hi MatchParen guibg=#2176FF guifg=#ffffff gui=bold
-  hi Normal guifg=#ffffff guibg=#000000
-  hi NormalSelected guifg=#50fa7b gui=bold
-  hi NvimTreeFolderIcon guibg=#34a1eb
-  hi PMenu guibg=#00509d guifg=#ffffff gui=bold
-  hi PMenuSel guibg=#34a1eb guifg=#000000 blend=0
-  hi Search guifg=#000000 guibg=#ff5555
-  hi SignColumn guibg=#000000 guifg=#34a1eb gui=bold
-  hi String guifg=#ff79c6 gui=bold
-  hi TelescopeBorder guifg=#34a1eb gui=bold
-  hi TelescopeNormal guibg=#000000
-  hi TelescopeMatching guifg=#ffffff gui=bold
-  hi TelescopeSelection guibg=#34a1eb guifg=#000000 gui=bold
-  hi javaScriptLineComment guifg=#34a1eb gui=italic
   hi link jsComment Comment
   hi link Searchlight IncSearch
   hi link TelescopeMultiSelection NormalSelected
@@ -37,7 +7,33 @@ vim.cmd([[
   hi link jsString String
 ]])
 
-  -- hi TabLine guibg=#000000 guifg=#ffff00 gui=NONE
-  -- hi TabLineModCur guibg=#50fa7b guifg=#000000 gui=bold
-  -- hi TabLineModNorm guibg=#6272a4 guifg=#ffffff gui=bold
-  -- hi TabLineSel guibg=#34a1eb guifg=#000000
+local set = vim.api.nvim_set_hl
+
+set(0, 'Comment', { fg = "#34a1eb", bold = true })
+set(0, 'CursorLine', { bg = "#00509d" })
+set(0, 'CursorLineNr', { fg = "#ffff00", bg = "#000000", bold = true })
+set(0, 'CursorWord', { underline = true })
+set(0, 'Folded', { bg = "#34a1eb", fg = "#000000" })
+set(0, 'GitSignsAdd', { fg = "#50fa7b", bg = "#000000", bold = true })
+set(0, 'GitSignsChange', { fg = "#ff79c6", bg = "#000000", bold = true })
+set(0, 'GitSignsDelete', { fg = "#ff5555", bg = "#000000", bold = true })
+set(0, 'HopNextKey', { fg = "#50fa7b" })
+set(0, 'HopNextKey1', { fg = "#50fa7b" })
+set(0, 'HopNextKey2', { fg = "#50fa7b" })
+set(0, 'IncSearch', { fg = "#000000", bg = "#50fa7b", bold = true })
+set(0, 'LineNr', { fg = "#34a1eb", bg = "#000000", bold = true })
+set(0, 'LspSignatureActiveParameter', { fg = "#000000", bg = "#34a1eb", bold = true })
+set(0, 'MatchParen', { bg = "#2176FF", fg = "#ffffff", bold = true })
+set(0, 'Normal', { fg = "#ffffff", bg = "#000000" })
+set(0, 'NormalSelected', { fg = "#50fa7b", bold = true })
+set(0, 'NvimTreeFolderIcon', { bg = "#34a1eb" })
+set(0, 'PMenu', { bg = "#00509d", fg = "#ffffff", bold = true })
+set(0, 'PMenuSel', { bg = "#34a1eb", fg = "#000000" })
+set(0, 'Search', { fg = "#000000", bg = "#ff79c6", bold = true })
+set(0, 'SignColumn', { bg = "#000000", fg = "#34a1eb", bold = true })
+set(0, 'String', { fg = "#ff79c6", bold = true })
+set(0, 'TelescopeBorder', { fg = "#34a1eb", bold = true })
+set(0, 'TelescopeNormal', { bg = "#000000" })
+set(0, 'TelescopeMatching', { fg = "#ffffff", bold = true })
+set(0, 'TelescopeSelection', { bg = "#34a1eb", fg = "#000000", bold = true })
+set(0, 'javaScriptLineComment', { fg = "#34a1eb", italic = true })
