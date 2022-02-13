c23aab1375e68fa944cd4b0875e952eedd3d8c2e
shlomo <shlomo.carmeter@gmail.com>
2022-02-13 14:17:44 +0200

    nvim: tabline config show filename only, changed modified icon

--- nvim/lua/config/tabline.lua
+++ nvim/lua/config/tabline.lua
@@ -2,7 +2,8 @@ require('tabline').setup({
   enable = true,
   options = {
     show_tabs_always = false,
-    tabline_show_filename_only = true,
-    tabline_show_bufnr = true
+    show_filename_only = true,
+    tabline_show_bufnr = true,
+    modified_icon = "[+]",
   }
 })
