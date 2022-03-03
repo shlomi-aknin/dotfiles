dbc967ab9d977382f4213a856e461467784d23e9
shlomo <shlomo.carmeter@gmail.com>
2022-03-03 15:31:21 +0200

    nvim: autoload all config files

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,28 +1,29 @@
--- require('config/autosave')
-require('config/cutlass')
-require('config/format')
-require('config/gitsigns')
-require('config/goto-preview')
-require('config/hop')
-require('config/lualine')
-require('config/lsp-colors')
-require('config/lsp_signature')
-require('config/neoscroll')
-require('config/nvim-colorizer')
-require('config/nvim-comment')
-require('config/nvim-cmp')
-require('config/nvim-dap')
-require('config/nvim-dap-ui')
-require('config/nvim-lspconfig')
-require('config/nvim-tree')
-require('config/nvim-treesitter')
-require('config/nvim-spectre')
-require('config/surround')
-require('config/tabline')
-require('config/telescope')
-require('config/toggle-lsp-diagnostics')
-require('config/toggleterm')
-require('config/trim')
-require('config/trouble')
-require('config/vim-vsnip')
-require('config/qf_helper')
+-- Require all other `.lua` files in the same directory
+
+local info = debug.getinfo(1, 'S')
+local module_directory = string.match(info.source, '^@(.*)/')
+local module_filename = string.match(info.source, '/([^/]*)$')
+
+local module_name = ... or 'config'
+
+local function scandir(directory)
+  local i, t, popen = 0, {}, io.popen
+  local pfile = popen('ls -a "' .. directory .. '"')
+  for filename in pfile:lines() do
+    i = i + 1
+    t[i] = filename
+  end
+  pfile:close()
+  return t
+end
+
+local config_files = vim.tbl_filter(function (filename)
+  local is_lua_module = string.match(filename, '[.]lua$')
+  local is_this_file = filename == module_filename
+  return is_lua_module and not is_this_file
+end, scandir(module_directory))
+
+for i, filename in ipairs(config_files) do
+  local config_module = string.match(filename, '(.+).lua$')
+  require(module_name .. '.' .. config_module)
+end
