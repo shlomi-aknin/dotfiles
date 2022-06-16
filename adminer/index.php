1a52789a906b505f6a5d253776274949459c387b
Shlomi Aknin <shlomi.aknin@gmail.com>
2022-06-16 11:10:11 +0300

    adminer: added adminer

---
+++ adminer/index.php
@@ -0,0 +1,37 @@
+<?php
+function adminer_object() {
+    // required to run any plugin
+    include_once "./plugins/plugin.php";
+
+    // autoloader
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+
+    // enable extra drivers just by including them
+    //~ include "./plugins/drivers/simpledb.php";
+
+    $plugins = array(
+        new AdminerTablesFilter(),
+        new AdminerDumpJson()
+        // specify enabled plugins here
+        // new AdminerDumpXml(),
+        // new AdminerTinymce(),
+        // new AdminerFileUpload("data/"),
+        // new AdminerSlugify(),
+        // new AdminerTranslation(),
+        // new AdminerForeignSystem(),
+    );
+
+    /* It is possible to combine customization and plugins:
+    class AdminerCustomization extends AdminerPlugin {
+    }
+    return new AdminerCustomization($plugins);
+    */
+
+    return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include "./adminer.php";
+?>
