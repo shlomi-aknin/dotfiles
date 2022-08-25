5fa02ba3a5bc5a715200c10ed23ecd16d2b4c801
Shlomi Aknin <shlomi.aknin@gmail.com>
2022-08-25 20:41:50 +0300

    nvim: changed mulit select code

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -4,6 +4,7 @@ if not status_ok then
 end
 
 local actions = require('telescope.actions')
+local transform_mod = require('telescope.actions.mt').transform_mod
 local action_state = require('telescope.actions.state')
 local action_layout = require('telescope.actions.layout')
 
@@ -21,22 +22,119 @@ local open_filtered = function(prompt_bufnr)
   vim.cmd('stopinsert')
 end
 
-function multi_select(prompt_bufnr)
-  local picker = action_state.get_current_picker(prompt_bufnr)
-  local num_selections = #picker:get_multi_selection()
-  if num_selections > 1 then
-    vim.cmd('bw!')
-    for _, entry in ipairs(picker:get_multi_selection()) do
-      vim.cmd(string.format('%s %s', ':e!', entry.value))
-      vim.cmd('luafile ~/.config/nvim/lua/settings.lua')
-      vim.cmd('luafile ~/.config/nvim/lua/scheme.lua')
-      vim.cmd('luafile ~/.config/nvim/lua/vimscript.lua')
+local function multiopen(prompt_bufnr, method)
+    local edit_file_cmd_map = {
+        vertical = "vsplit",
+        horizontal = "split",
+        tab = "tabedit",
+        default = "edit",
+    }
+    local edit_buf_cmd_map = {
+        vertical = "vert sbuffer",
+        horizontal = "sbuffer",
+        tab = "tab sbuffer",
+        default = "buffer",
+    }
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local multi_selection = picker:get_multi_selection()
+
+    if #multi_selection > 1 then
+        require("telescope.pickers").on_close_prompt(prompt_bufnr)
+        pcall(vim.api.nvim_set_current_win, picker.original_win_id)
+
+        for i, entry in ipairs(multi_selection) do
+            local filename, row, col
+
+            if entry.path or entry.filename then
+                filename = entry.path or entry.filename
+
+                row = entry.row or entry.lnum
+                col = vim.F.if_nil(entry.col, 1)
+            elseif not entry.bufnr then
+                local value = entry.value
+                if not value then
+                    return
+                end
+
+                if type(value) == "table" then
+                    value = entry.display
+                end
+
+                local sections = vim.split(value, ":")
+
+                filename = sections[1]
+                row = tonumber(sections[2])
+                col = tonumber(sections[3])
+            end
+
+            local entry_bufnr = entry.bufnr
+
+            if entry_bufnr then
+                if not vim.api.nvim_buf_get_option(entry_bufnr, "buflisted") then
+                    vim.api.nvim_buf_set_option(entry_bufnr, "buflisted", true)
+                end
+                local command = i == 1 and "buffer" or edit_buf_cmd_map[method]
+                pcall(vim.cmd, string.format("%s %s", command, vim.api.nvim_buf_get_name(entry_bufnr)))
+            else
+                local command = i == 1 and "edit" or edit_file_cmd_map[method]
+                if vim.api.nvim_buf_get_name(0) ~= filename or command ~= "edit" then
+                    filename = require("plenary.path"):new(vim.fn.fnameescape(filename)):normalize(vim.loop.cwd())
+                    pcall(vim.cmd, string.format("%s %s", command, filename))
+                end
+            end
+
+            if row and col then
+                pcall(vim.api.nvim_win_set_cursor, 0, { row, col-1 })
+            end
+        end
+    else
+        actions["select_" .. method](prompt_bufnr)
     end
-    vim.cmd('stopinsert')
-  else
-    actions.select_default(prompt_bufnr)
-  end
 end
+
+local custom_actions = transform_mod({
+    multi_selection_open_vertical = function(prompt_bufnr)
+        multiopen(prompt_bufnr, 'vertical')
+    end,
+    multi_selection_open_horizontal = function(prompt_bufnr)
+        multiopen(prompt_bufnr, 'horizontal')
+    end,
+    multi_selection_open_tab = function(prompt_bufnr)
+        multiopen(prompt_bufnr, 'tab')
+    end,
+    multi_selection_open = function(prompt_bufnr)
+        multiopen(prompt_bufnr, 'default')
+    end,
+})
+
+local function stopinsert(callback)
+    return function(prompt_bufnr)
+        vim.cmd.stopinsert()
+        vim.schedule(function()
+            callback(prompt_bufnr)
+        end)
+    end
+end
+
+local multi_open_mappings = {
+    i = {
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-l>']  = stopinsert(custom_actions.multi_selection_open),
+        ['<C-v>'] = stopinsert(custom_actions.multi_selection_open_vertical),
+        ['<C-s>'] = stopinsert(custom_actions.multi_selection_open_horizontal),
+        ['<C-t>'] = stopinsert(custom_actions.multi_selection_open_tab),
+        ['<CR>']  = stopinsert(custom_actions.multi_selection_open)
+    },
+    n = {
+        ['<C-c>'] = actions.close,
+        ['<C-l>'] = custom_actions.multi_selection_open,
+        ['<C-v>'] = custom_actions.multi_selection_open_vertical,
+        ['<C-s>'] = custom_actions.multi_selection_open_horizontal,
+        ['<C-t>'] = custom_actions.multi_selection_open_tab,
+        ['<CR>'] = custom_actions.multi_selection_open,
+    },
+}
 
 function yank_file()
   local entry = action_state.get_selected_entry()
@@ -58,31 +156,8 @@ telescope.setup({
     layout_config = {
       preview_width = 0.6
     },
-    file_ignore_patterns = { 'node_modules', '.git', 'package.json', 'package-lock.json' },
-    mappings = {
-      i = {
-        ['<esc>'] = actions.close,
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous,
-        ['<C-y>'] = yank_file,
-        ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
-        ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
-        ['<cr>'] = multi_select,
-        ['<C-l>'] = multi_select,
-        ['<C-h>'] = action_layout.toggle_preview,
-        ['<C-o>'] = open_filtered,
-        ['<C-q>'] = actions.smart_send_to_qflist,
-      },
-      n = {
-        ['<esc>'] = actions.close,
-        ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
-        ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
-        ['<cr>'] = multi_select,
-        ['<C-l>'] = multi_select,
-        ['<C-h>'] = action_layout.toggle_preview,
-        ['<C-q>'] = actions.smart_send_to_qflist,
-      }
-    },
+    file_ignore_patterns = { 'node_modules', '.git', 'package-lock.json' },
+    mappings = multi_open_mappings
   }
 })
 
