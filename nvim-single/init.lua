1b903bafeda1c82cc9d570360dea63b63b556850
Shlomi Aknin <shlomi.aknin@gmail.com>
2022-12-20 00:46:18 +0200

    nvim: added new single file init

---
+++ nvim-single/init.lua
@@ -0,0 +1,56 @@
+-- Options
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.termguicolors = true
+vim.o.cursorline = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.clipboard = 'unnamedplus'
+vim.o.completeopt = 'menuone,preview'
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.nofoldenable = true
+
+-- Packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'Mofiqul/dracula.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+-- Colorscheme
+vim.cmd[[colorscheme dracula]]
+
+-- Treesitter
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = 'all',
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true
+  }
+}
