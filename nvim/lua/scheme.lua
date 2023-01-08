52b2561e7affd94db8d8b3d0ec9161c82e09b2d5
Shlomi Aknin <shlomi.aknin@gmail.com>
2023-01-08 23:20:19 +0200

    nvim: scheme changes

--- nvim/lua/scheme.lua
+++ nvim/lua/scheme.lua
@@ -1,13 +1,13 @@
 require("tokyonight").setup({
-  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
     comments = { italic = true },
-    keywords = { italic = true },
-    functions = { italic = true },
-    variables = { italic = true },
+    keywords = { bold = true },
+    functions = { bold = true },
+    variables = { bold = true },
     -- Background styles. Can be "dark", "transparent" or "normal"
     sidebars = "dark", -- style for sidebars, see below
     floats = "dark", -- style for floating windows
