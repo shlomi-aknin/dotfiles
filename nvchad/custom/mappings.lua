7313e6f375828206424f48913acc869ba6daaec0
Shlomo Aknin <shlomi.aknin@gmail.com>
2023-10-18 09:33:39 +0300

    nvchad: add silent option on mappings

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -3,22 +3,22 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>s"] = { ":w<cr>", "Save",opts = { nowait = true } },
-    ["<leader>e"] = { ":NvimTreeToggle<cr>", "Toggle Nvim Tree",opts = { nowait = true } },
-    ["<C-l>"] = { ":bn<cr>", "Toggle Nvim Tree",opts = { nowait = true } },
-    ["<C-h>"] = { ":bp<cr>", "Toggle Nvim Tree",opts = { nowait = true } },
-    ["<C-j>"] = { "yyp", "Copy line down",opts = { nowait = true } },
-    ["<C-k>"] = { "yyP", "Copy line up",opts = { nowait = true } },
-    ["<leader>bb"] = { ":Telescope buffers<cr>", "Find open buffer",opts = { nowait = true } },
-    ["<leader>qq"] = { ":bd!<cr>", "Delete buffer",opts = { nowait = true } },
-    ["H"] = { "^", "Goto first non blank",opts = { nowait = true } },
-    ["L"] = { "g_", "Goto last non blank",opts = { nowait = true } },
-    ["gv"] = { "Vf{%", "Goto last non blank",opts = { nowait = true } },
-  },
+    [";"] = { ":", "enter command mode", opts = { nowait = true, silent = true } },
+    ["<leader>s"] = { ":w<cr>", "Save",opts = { nowait = true, silent = true } },
+    ["<leader>e"] = { ":NvimTreeToggle<cr>", "Toggle Nvim Tree",opts = { nowait = true, silent = true } },
+    ["<C-l>"] = { ":bn<cr>", "Toggle Nvim Tree",opts = { nowait = true, silent = true } },
+    ["<C-h>"] = { ":bp<cr>", "Toggle Nvim Tree",opts = { nowait = true, silent = true } },
+    ["<C-j>"] = { "yyp", "Copy line down",opts = { nowait = true, silent = true } },
+    ["<C-k>"] = { "yyP", "Copy line up",opts = { nowait = true, silent = true } },
+    ["<leader>bb"] = { ":Telescope buffers<cr>", "Find open buffer",opts = { nowait = true, silent = true } },
+    ["<leader>qq"] = { ":bd!<cr>", "Delete buffer",opts = { nowait = true, silent = true } },
+    ["H"] = { "^", "Goto first non blank",opts = { nowait = true, silent = true } },
+    ["L"] = { "g_", "Goto last non blank",opts = { nowait = true, silent = true } },
+    ["gv"] = { "Vf{%", "Goto last non blank",opts = { nowait = true, silent = true } },
+ },
   v = {
     [">"] = { ">gv", "indent"},
-  },
+ },
 }
 
 -- more keybinds!
