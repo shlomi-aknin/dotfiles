3f7059758abac068028c2b7c9877fcc01533d1f3
Shlomo Aknin <shlomi.aknin@gmail.com>
2023-11-14 11:18:37 +0200

    nvchad: added nvim tree keymaps

---
+++ nvchad/custom/configs/nvim-tree.lua
@@ -0,0 +1,49 @@
+local M = {}
+local api = require("nvim-tree.api")
+
+local function edit_or_open()
+  local node = api.tree.get_node_under_cursor()
+
+  if node.nodes ~= nil then
+    -- expand or collapse folder
+    api.node.open.edit()
+  else
+    -- open file
+    api.node.open.edit()
+    -- Close the tree if file was opened
+    api.tree.close()
+  end
+end
+
+-- open as vsplit on current node
+local function vsplit_preview()
+  local node = api.tree.get_node_under_cursor()
+
+  if node.nodes ~= nil then
+    -- expand or collapse folder
+    api.node.open.edit()
+  else
+    -- open file as vsplit
+    api.node.open.vertical()
+  end
+
+  -- Finally refocus on tree if it was lost
+  api.tree.focus()
+end
+
+M.nvimtree_on_attach = function(bufnr)
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set("n", "l", edit_or_open,          opts("Edit Or Open"))
+  vim.keymap.set("n", "L", vsplit_preview,        opts("Vsplit Preview"))
+  vim.keymap.set("n", "h", edit_or_open,          opts("Close"))
+  vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+end
+
+return M
---
+++ nvchad/custom/configs/overrides.lua
@@ -0,0 +1,60 @@
+local M = {}
+local nvimtree_custom = require("custom.configs.nvim-tree")
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+  on_attach = nvimtree_custom.nvimtree_on_attach,
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
