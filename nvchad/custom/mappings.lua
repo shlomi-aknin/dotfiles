c36618b9557dfb165bd14935317757dcd8bd62b9
Shlomo Aknin <shlomi.aknin@gmail.com>
2023-11-15 16:01:26 +0200

    nvchad: quickfix open and clear highlight on hjkl

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,9 +1,19 @@
 ---@type MappingsTable
 local M = {}
 
+-- Function to toggle Quickfix window open and close
+function ToggleQuickfix()
+    if vim.fn.empty(vim.fn.getqflist()) == 0 then
+        -- Quickfix window is open, so close it
+        vim.cmd("cclose")
+    else
+        -- Quickfix window is closed, so open it
+        vim.cmd("copen")
+    end
+end
+
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true, silent = true } },
     ["<C-t>"] = { ":enew<cr>", "New Buffer", opts = { nowait = true, silent = true } },
     ["<leader>j"] = { "o<esc>", "Add line below", opts = { nowait = true, silent = true } },
     ["<leader>k"] = { "O<esc>", "Add line above", opts = { nowait = true, silent = true } },
@@ -14,8 +24,15 @@ M.general = {
     ["<C-j>"] = { "yyp", "Copy line down", opts = { nowait = true, silent = true } },
     ["<C-k>"] = { "yyP", "Copy line up", opts = { nowait = true, silent = true } },
     ["<leader>b"] = { ":Telescope buffers<cr>", "Find open buffer", opts = { nowait = true, silent = true } },
+    ["<leader>d"] = { ":Telescope diagnostics<cr>", "Diagnostics", opts = { nowait = true, silent = true } },
     ["<leader>f"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
+    ["h"] = { "<cmd> noh <CR>h", "Clear highlight on movement", opts = { nowait = true, silent = true } },
+    ["j"] = { "<cmd> noh <CR>j", "Clear highlight on movement", opts = { nowait = true, silent = true } },
+    ["k"] = { "<cmd> noh <CR>k", "Clear highlight on movement", opts = { nowait = true, silent = true } },
+    ["l"] = { "<cmd> noh <CR>l", "Clear highlight on movement", opts = { nowait = true, silent = true } },
     ["gs"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+    ["gS"] = { "<cmd> Telescope grep_string <CR>", "Live grep" },
+    ["<leader>qw"] = { ':lua ToggleQuickfix()<CR>', "Quickfix toggle", opts = { noremap = true, nowait = true, silent = true } },
     ["<leader>qq"] = { ":bd!<cr>", "Delete buffer", opts = { nowait = true, silent = true } },
     ["<leader>x"] = { ":%bd!<cr>", "Delete all buffer", opts = { nowait = true, silent = true } },
     ["H"] = { "^", "Goto first non blank", opts = { nowait = true, silent = true } },
@@ -24,6 +41,7 @@ M.general = {
  },
   v = {
     [">"] = { ">gv", "indent"},
+    ["gS"] = { "<cmd> Telescope grep_string <CR>", "Live grep" },
  },
 }
 
