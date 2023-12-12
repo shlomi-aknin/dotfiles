27b58828a9a8e03c889c97c0766ccaf9cb507f1d
Shlomo Aknin <shlomi.aknin@gmail.com>
2023-12-13 00:41:35 +0200

    nvim: added telescope

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 
 local keymap = vim.keymap
 
+keymap.set('n', 'gv', 'Vg_%') 
 keymap.set('n', 'H', '^') 
 keymap.set('n', 'L', 'g_') 
 keymap.set('n', '<C-h>', ':bp<CR>') 
@@ -9,7 +10,9 @@ keymap.set('n', '<C-j>', 'yyp')
 keymap.set('n', '<C-k>', 'yyP') 
 keymap.set('n', '<C-l>', ':bn<CR>') 
 keymap.set('n', '<C-t>', ':enew<CR>') 
+keymap.set('n', '<leader>b', ':Telescope buffers<CR>') 
 keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>') 
+keymap.set('n', '<leader>f', ':Telescope find_files<CR>') 
 keymap.set('n', '<leader>j', 'o<ESC>') 
 keymap.set('n', '<leader>k', 'O<ESC>') 
 keymap.set('n', '<leader>qq', ':bd!<CR>') 
