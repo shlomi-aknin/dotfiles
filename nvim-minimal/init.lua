0748e21441347bf54596915628fa3087321ea520
Shlomo Aknin <shlomi.aknin@gmail.com>
2024-02-08 11:28:39 +0200

    nvim: minimal config; vifm: use minimal nvim config

---
+++ nvim-minimal/init.lua
@@ -0,0 +1,14 @@
+-- Enable cursorline
+vim.opt.cursorline = true
+
+-- Show line numbers
+vim.opt.number = true
+
+-- Show relative line numbers
+vim.opt.relativenumber = true
+
+-- Use system clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- Enable termguicolors
+vim.opt.termguicolors = true
