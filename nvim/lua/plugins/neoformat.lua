b3189032005d4cdb34d0d6351b6493d50f771c28
Shlomo Aknin <shlomi.aknin@gmail.com>
2024-09-18 00:53:36 +0300

    nvim: neoformat restore undojoin

--- nvim/lua/plugins/neoformat.lua
+++ nvim/lua/plugins/neoformat.lua
@@ -1,16 +1,12 @@
 return {
   'sbdchd/neoformat',
   config = function()
-    -- autocmd BufWritePre * undojoin | Neoformat prettierd | Neoformat eslint_d
-
     vim.cmd[[
       augroup fmt
       autocmd!
-      autocmd BufWritePre * undojoin | Neoformat
+      autocmd BufWritePre *.js,*.ts undojoin | Neoformat
       augroup END
     ]]
-
-    -- vim.g.neoformat_enabled_typescript = { 'eslint_d', 'prettierd' }
   end,
   opts = {},
 }
