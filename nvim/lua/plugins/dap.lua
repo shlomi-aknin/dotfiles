ce13fc1c02a20fd7167aeb8c87c9bdebd559a95a
Shlomo Aknin <shlomi.aknin@gmail.com>
2024-09-17 09:07:34 +0300

    nvim: added debug test with dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -271,7 +271,35 @@ return {
       },
     },
     dependencies = {
-      { "rcarriga/nvim-dap-ui", opts = {}, dependencies = { "nvim-neotest/nvim-nio" } },
+      { "rcarriga/nvim-dap-ui", opts = {
+        layouts = { {
+        elements = { {
+            id = "scopes",
+            size = 0.25
+          }, {
+            id = "watches",
+            size = 0.25
+          }, {
+            id = "breakpoints",
+            size = 0.25
+          }, {
+            id = "stacks",
+            size = 0.25
+          } },
+        position = "left",
+        size = 40
+      }, {
+        elements = { {
+            id = "repl",
+            size = 0.5
+          }, {
+            id = "console",
+            size = 0.5
+          } },
+        position = "bottom",
+        size = 10
+      } },
+      }, dependencies = { "nvim-neotest/nvim-nio" } },
       { "theHamsta/nvim-dap-virtual-text", opts = { enabled = true, virt_text_pos = 'inline' } },
       -- Install the vscode-js-debug adapter
       {
